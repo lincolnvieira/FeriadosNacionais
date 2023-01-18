e2579da84817b709aa9643b5c2b1484f091baa35
Lincoln Vieira <lincoln_vieira@outlook.com.br>
2023-01-18 11:55:32 -0300

    Criação inicial dos componentes de lista e edição

--- Holidays-Client/src/app/app.module.ts
+++ Holidays-Client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditComponent } from './national-holidays/edit/edit.component';
+import { ListComponent } from './national-holidays/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
