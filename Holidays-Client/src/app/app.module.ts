6eebc8c6b740007a0bc5ea9f8ddc9a4fd6e81435
Lincoln Vieira <lincoln_vieira@outlook.com.br>
2023-01-18 12:37:08 -0300

    Montagem das rotas rotas e componente home

--- Holidays-Client/src/app/app.module.ts
+++ Holidays-Client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditComponent } from './national-holidays/edit/edit.component';
 import { ListComponent } from './national-holidays/list/list.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EditComponent,
-    ListComponent
+    ListComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
